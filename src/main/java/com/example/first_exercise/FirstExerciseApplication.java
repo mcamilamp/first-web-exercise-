92cc6629b886ac302d2ebd14d942e3bc3e583fde
mcamilamp <camimerpa@gmail.com>
2024-09-03 13:02:08 -0500

    first commit

---
+++ src/main/java/com/example/first_exercise/FirstExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.first_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstExerciseApplication.class, args);
+	}
+
+}
